cd8bf093753fa0187c9516f8c4753f1bbdb51468
aidoctor <aidoctor@cafe24.com>
2020-10-17 20:52:57 +0900

    create mongodb test4

--- config/db_config_mongo.js
+++ config/db_config_mongo.js
@@ -87,7 +87,7 @@ module.exports = function () {
         },
         mongo_find: async (col_name, projection, query) => { // id : 사용자 아이디, col_name : 컬렉션 네임, query : 문자열 쿼리, projection : 나올 컬럼
             client = MongoClient.connect(
-                'mongodb://' + config.user + ':' + config.password + '@' + config.host + '/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
+                'mongodb://admin0:admin00!!@aidoctor-docdb.cluster-ckhpnljabh2s.us-west-2.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
                 {
                     sslValidate: true,
                     sslCA: ca,
