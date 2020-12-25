e28c5b4f6a7b577a4daf332efbffa66240fc78ca
pu1et <pu1etproof@gmail.com>
2020-12-25 23:33:20 +0900

    test

--- web.js
+++ web.js
@@ -4,7 +4,6 @@ const bodyParser= require('body-parser');
 const app = express();
 
 route = require('./route/routes.js');
-  
 app.set('port',process.env.PORT || 1337);
 
 app.use(express.static('public'));
